4b49e1869e1949578481a5df1373a6bc564f1bf3
Jiyavur <jiyavurrahman80@gmail.com>
2026-05-22 08:59:33 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -31,7 +31,8 @@ function App() {
         <h4>Available For Work</h4>
       </div>
 
-      <div className="hero-body">
+      <div className="hero-body flex justify-between align-center">
+        <div className="hero-left">
         <div className="hero-head">
           <span>Crafting </span>
           <span>digital </span>
@@ -47,9 +48,39 @@ function App() {
           <p>I turn complex problems into elegant solutions.</p>
         </div>
 
-        <div className="hero-buttons">
-          <button>View Projects</button>
-          <button>Github Activity</button>
+        <div className="hero-buttons flex gap-6">
+          <button className="b1">View Projects</button>
+          <button className="b2">Github Activity ↓</button>
+        </div>
+        </div>
+        
+        <div className="hero-right pr-[3rem] pt-[3.5rem]">
+          <div className="code-card flex flex-col align-center gap-3">
+
+           <div className="cchead flex gap-2">
+            <span className="cch1"></span>
+            <span className="cch2"></span>
+            <span className="cch3"></span>
+           </div>
+
+           <div className="ccbody">
+            <p><span className="sno">01</span>     <span className="var-name text-fuchsia-300">const</span> <span className="varia">developer</span><span className="text-[white]">= {"{"}</span></p>
+            <p><span className="sno">02</span>     <span className="objkey">name:</span><span className="objval">"Your name",</span></p>
+            <p><span className="sno">03</span>     <span className="objkey">role:</span><span className="objval">"Frontend Developer",</span></p>
+            <p><span className="sno">04</span>     <span className="objkey">passion:</span><span className="objval">"Problem Solving",</span></p>
+            <p><span className="sno">05</span>     <span className="objkey">skills:</span><span className="objval">["React","Tailwins css"],</span></p>
+            <p><span className="sno">06</span>     <span className="objkey">available:</span><span className="objval">true,</span></p>
+            <p><span className="sno">07</span>    {"}"};</p>
+            <p><span className="sno">08</span></p>
+            <p><span className="sno">09</span>     <span className="commentt text-[var(--muted)]">// Let's build something</span></p>
+            <p><span className="sno">10</span>     <span className="commentt text-[var(--muted)]">// incredible together! 🚀</span></p>
+           </div>
+
+           <div className="cctail">
+
+           </div>
+
+          </div>
         </div>
 
       </div>
